d2741355d08aa9a1e4795f2df9b3b364a4868f77
Nikita Belov <nikita2010belov@gmail.com>
2026-02-01 12:11:13 +0500

    Добавлены кнопки и отмена ожидания поста

--- main.py
+++ main.py
@@ -1,11 +1,16 @@
-import random
+import random, json
 from datetime import datetime, timezone
+
+from config import VKADMIN_ID, VKTOKEN, TGCHAT, TGTOKEN #config.py
+
+#import for telegram
+import telebot;
+tgbot = telebot.TeleBot(TGTOKEN)
 
 #import for vk
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
 
-from config import VKADMIN_ID, VKTOKEN, TGCHAT, TGTOKEN #config.py
 from vkbot import VkBot
 
 def write_msg(user_id, message, key):
@@ -18,9 +23,37 @@ def write_msg(user_id, message, key):
 vk = vk_api.VkApi(token=VKTOKEN)
 longpoll = VkLongPoll(vk)
 
-#import for telegram
-import telebot;
-tgbot = telebot.TeleBot(TGTOKEN)
+main_keyboard = {
+    "one_time": False,
+    "buttons": [
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"1\"}",
+                "label": "Отправить"
+            },
+            "color": "positive"
+        }]
+    ]
+}
+main_keyboard = json.dumps(main_keyboard, ensure_ascii=False).encode('utf-8')
+main_keyboard = str(main_keyboard.decode('utf-8'))
+
+cancel_keyboard = {
+    "one_time": True,
+    "buttons": [
+        [{
+            "action": {
+                "type": "text",
+                "payload": "{\"button\": \"1\"}",
+                "label": "Отмена"
+            },
+            "color": "negative"
+        }]
+    ]
+}
+cancel_keyboard = json.dumps(cancel_keyboard, ensure_ascii=False).encode('utf-8')
+cancel_keyboard = str(cancel_keyboard.decode('utf-8'))
 
 
 try:
@@ -33,23 +66,30 @@ try:
                 if event.user_id == VKADMIN_ID:
                     if event.text.lower() == "отправить":
                         # write_msg(event.user_id, f"{event.text}", 0)
-                        write_msg(event.user_id, "Ожидаю текст", 0)
+                        write_msg(event.user_id, "Ожидаю текст", cancel_keyboard)
                         for event in longpoll.listen():
                             if event.type == VkEventType.MESSAGE_NEW:
                                 if event.to_me:
+                                    if event.text.lower() == "отмена":
+                                        write_msg(event.user_id, "Отмена пересылки", main_keyboard)
+                                        break
                                     current_time_utc = datetime.now(timezone.utc)
                                     formatted_time = current_time_utc.strftime('%d.%m.%Y %H:%M:%S UTC+0')
                                     debug_text = f"--------\n[{formatted_time}] FORWARDED FROM {event.user_id}:\n{event.text}\n--------\n\n"
 
                                     #output forwarded text
                                     tgbot.send_message(TGCHAT, event.text)
-                                    write_msg(event.user_id, "Отправлено!", 0)
+                                    write_msg(event.user_id, "Отправлено!", main_keyboard)
                                     print(debug_text)
 
                                     break
 
+
+                    # elif event.text.lower() == "test":
+                    #     write_msg(event.user_id, f"{event.user_id}", 0)
+
                     else:
-                        write_msg(event.user_id, f"{event.text}", 0)
+                        write_msg(event.user_id, f"{event.text}", main_keyboard)
                 
                 else:
                     write_msg(event.user_id, "У вас нет доступа к этому боту", 0)
